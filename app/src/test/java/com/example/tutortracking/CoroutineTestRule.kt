cb6e4af422b71af59d1356b35be727b68db8d075
astro <ahmed123.as27@gmail.com>
2021-10-20 03:41:39 +0400

    repo and viewModel test cases are done

--- app/src/test/java/com/example/tutortracking/CoroutineTestRule.kt
+++ app/src/test/java/com/example/tutortracking/CoroutineTestRule.kt
@@ -1,29 +1,26 @@
 package com.example.tutortracking
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class MainCoroutineRoleAndroid(
-    private val dispatcher: CoroutineDispatcher = TestCoroutineDispatcher()
-) : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher){
+class CoroutinesTestRule(
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+) : TestWatcher() {
 
     override fun starting(description: Description?) {
         super.starting(description)
-        Dispatchers.setMain(dispatcher)
+        Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description?) {
         super.finished(description)
-        cleanupTestCoroutines()
         Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
     }
-
 }
